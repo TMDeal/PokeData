4fc41cf146c2642c8cef303b42581638ab669ead
Trent Deal <trent.deal@gmail.com>
2017-02-01 13:45:39 -0500

    Add header component to App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     NgbModule.forRoot(),
