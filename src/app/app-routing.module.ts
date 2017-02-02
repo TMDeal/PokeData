56f4d2db7e5de6c882ed7d4045992955f5737653
Trent Deal <trent.deal@gmail.com>
2017-02-02 02:52:41 -0500

    Add routes to App
    No route has actual data on it right now. That will be added soon
    
    - Route for pokemon stats like IV's/EV's
    - Route for pokemon moves
    - route for pokemon abilities
    - Header links to routes correctly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { StatsComponent }from './stats/stats.component';
+import { MovesComponent } from './moves/moves.component';
+import { AbilitiesComponent } from './abilities/abilities.component';
+
 const routes: Routes = [
   {
     path: '',
     children: []
+  },
+  {
+    path: 'stats',
+    children: [
+      { path: '', component: StatsComponent }
+    ]
+  },
+  {
+    path: 'moves',
+    children: [
+      { path: '', component: MovesComponent }
+    ]
+  },
+  {
+    path: 'abilities',
+    children: [
+      { path: '', component: AbilitiesComponent }
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './header/header.component';
+import { StatsComponent } from './stats/stats.component';
+import { MovesComponent } from './moves/moves.component';
+import { AbilitiesComponent } from './abilities/abilities.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StatsComponent,
+    MovesComponent,
+    AbilitiesComponent,
+    PokemonComponent
   ],
   imports: [
     NgbModule.forRoot(),
