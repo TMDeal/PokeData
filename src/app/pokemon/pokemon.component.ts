3f7e0bd6b9732c9ef5ff3cd6a540e2b52eaea99f
Trent Deal <trent.deal@gmail.com>
2017-02-04 19:14:44 -0500

    Add pokemon list view

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../shared/services/pokemon.service';
+
+import { PokemonPage } from '../shared/interfaces/pokemon-page';
 
 @Component({
   selector: 'app-pokemon',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.scss']
 })
 export class PokemonComponent implements OnInit {
+  private page: PokemonPage;
+  private currentPage = 1;
+  private pageSize = 12;
+  private errorMsg: string;
+  private next: string;
+  private previous: string;
 
-  constructor() { }
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit() {
+    this.pokemonService.getPage(this.pageSize)
+      .subscribe(
+        page => this.page = page,
+        error => this.errorMsg = <any>error,
+        () => console.log('Pokemon page retrived')
+      );
   }
-
 }
