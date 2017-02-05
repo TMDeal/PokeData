fc7cb2d76a72498177bbefdcada4435ddaf36580
Trent Deal <trent.deal@gmail.com>
2017-02-04 19:07:52 -0500

    Make all routes children of the root route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { StatsComponent }from './stats/stats.component';
+import { StatsComponent } from './stats/stats.component';
 import { MovesComponent } from './moves/moves.component';
 import { AbilitiesComponent } from './abilities/abilities.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
-  },
-  {
-    path: 'stats',
     children: [
-      { path: '', component: StatsComponent }
+      {
+        path: '',
+        children: [
+          { path: '', component:  PokemonComponent}
+        ]
+      },
+      {
+        path: 'stats',
+        children: [
+          { path: '', component: StatsComponent }
+        ]
+      },
+      {
+        path: 'moves',
+        children: [
+          { path: '', component: MovesComponent }
+        ]
+      },
+      {
+        path: 'abilities',
+        children: [
+          { path: '', component: AbilitiesComponent }
+        ]
+      }
     ]
   },
-  {
-    path: 'moves',
-    children: [
-      { path: '', component: MovesComponent }
-    ]
-  },
-  {
-    path: 'abilities',
-    children: [
-      { path: '', component: AbilitiesComponent }
-    ]
-  }
 ];
 
 @NgModule({
