f0aa2e5112dbb62e2ee31be4c466a182433acfb4
Trent Deal <trent.deal@gmail.com>
2017-02-06 14:55:42 -0500

    move pokemon-page-item into pokemo-page
    PokemonPage is the only interface that made use of PokemonPageItem, So
    it has been moved into pokemon-page as PageItem

--- src/app/shared/interfaces/pokemon-page-item.ts
+++
@@ -1,5 +0,0 @@
-export interface PokemonPageItem {
-  id: number;
-  name: string;
-  url: string;
-}
--- src/app/shared/interfaces/pokemon-page.ts
+++ src/app/shared/interfaces/pokemon-page.ts
@@ -1,6 +1,12 @@
-import { PokemonPageItem } from './pokemon-page-item';
+import { Resource } from './common';
+
+interface PageItem extends Resource {
+  id: number;
+}
+
 export interface PokemonPage {
+  count: number;
   next: string;
   previous: string;
-  contents: PokemonPageItem[];
+  contents: PageItem[];
 }
