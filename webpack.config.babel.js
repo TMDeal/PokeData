467e85c804c6192a4d42f32a77c1cd658198bfb6
Trent Deal <trent.deal@gmail.com>
2017-10-18 19:21:15 -0400

    Setup webpack, build scripts, and babel

---
+++ webpack.config.babel.js
@@ -0,0 +1,43 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+const ROOT_DIR = path.resolve(__dirname);
+const DIST_DIR = path.resolve(ROOT_DIR, 'dist');
+const APP_DIR  = path.resolve(ROOT_DIR, 'src', 'app');
+
+export default {
+  entry: {
+    app: APP_DIR + '/index.js'
+  },
+
+  output: {
+    path: DIST_DIR,
+    filename: '[name].js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      },
+      {
+        test: /\.scss$|.sass$/,
+        include: path.resolve(APP_DIR, 'styles'),
+        use: [
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'PokeData',
+      template: path.resolve(ROOT_DIR, 'src', 'index.html')
+    })
+  ]
+};
