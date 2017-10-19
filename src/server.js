4490ec6299ac945cecda17f64445c2a452d60bb3
Trent Deal <trent.deal@gmail.com>
2017-10-19 16:03:34 -0400

    Move express static to top

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || '8080';
 const host = 'localhost';
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../dist')));
+
 if(process.env.NODE_ENV != 'production') {
   let config = webpackConfig({
     env: {
@@ -27,8 +29,6 @@ if(process.env.NODE_ENV != 'production') {
 
   app.use(webpackHotMiddleware(compiler));
 }
-
-app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen('8080', () => {
   console.log(`listening on ${host}:${port}`);
