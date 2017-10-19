1909fb1e7520cedf4d4b0078153908e20207f7ec
Trent Deal <trent.deal@gmail.com>
2017-10-19 13:14:29 -0400

    Use jsx extension

--- src/app/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { App } from 'components/app';
-
-const renderTarget = document.getElementById('app');
-
-ReactDOM.render(<App/>, renderTarget);
