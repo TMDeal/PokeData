e6116fff40565cffd8d3fb2ee04619066459bd88
Trent Deal <trent.deal@gmail.com>
2017-10-19 13:03:56 -0400

    Rename and add development settings
    There is no need for babel to be in the name, I found out.
    webpack not has hot reloading enabled and uses webpack-merge in order to
    combine the common config with config for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import merge from 'webpack-merge';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 
 const ROOT_DIR = path.resolve(__dirname);
 const DIST_DIR = path.resolve(ROOT_DIR, 'dist');
-const APP_DIR  = path.resolve(ROOT_DIR, 'src', 'app');
+const APP_DIR  = path.resolve(ROOT_DIR, 'src/app');
 
-export default {
+let config = {
   entry: {
-    app: APP_DIR + '/index.js'
+    bundle: [APP_DIR + '/index.jsx']
   },
 
   output: {
     path: DIST_DIR,
+    publicPath: '/',
     filename: '[name].js'
   },
 
@@ -48,3 +52,21 @@ export default {
     })
   ]
 };
+
+if(process.env.NODE_ENV != 'production') {
+
+  config = merge(config, {
+    devtool: 'inline-source-map',
+    entry: {
+      bundle: [
+        "webpack-hot-middleware/client",
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ]
+  });
+}
+
+export default config;
