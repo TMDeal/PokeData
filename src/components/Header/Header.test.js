91cdf3ef6cd9ece5fa35aaf3c2c8db64c887e0b7
Trent Deal <trent.deal@gmail.com>
2017-10-24 00:20:51 -0400

    Add new test for drawer

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import MenuItem from 'material-ui/MenuItem';
 import Header from './Header';
+
+let component;
+
+beforeEach(() => {
+  component = shallow(
+    <Header>
+      <MenuItem>Test</MenuItem>
+    </Header>
+  );
+})
 
 it('renders without crashing', () => {
   shallow(<Header/>);
 });
+
+it('should close the drawer when a link in the drawer is pressed', () => {
+  expect(component).toHaveState('drawerIsOpen');
+
+  component.setState({
+    drawerIsOpen: true
+  });
+
+  expect(component).toHaveState('drawerIsOpen', true);
+
+  let testItem = component.find(MenuItem);
+  expect(testItem).toBeDefined();
+
+  testItem.simulate('click');
+  expect(component).toHaveState('drawerIsOpen', false);
+})
