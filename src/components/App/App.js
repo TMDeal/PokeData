782a96b09723f8be88c03037d6aebf082ab90503
Trent Deal <trent.deal@gmail.com>
2017-10-24 00:18:38 -0400

    Update Header and App
    App supplies the header with the menuitems instead of the Header needing
    to. Header also does not use withRouter, because context. It also open
    a drawer when the left appbar icon is pressed. This closes on escape,
    link clicked, or clicked outside the drawer.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,26 @@
 // @flow
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import MenuItem from 'material-ui/MenuItem';
 import Header from '../Header';
 
 class App extends React.Component<{}> {
   render() {
     return (
-      <Header/>
+      <div className='container'>
+        <Header>
+          <MenuItem
+            containerElement={<NavLink to='#pokemon'></NavLink>}
+          >
+            Pokemon
+          </MenuItem>
+          <MenuItem
+            containerElement={<NavLink to='#moves'></NavLink>}
+          >
+            Moves
+          </MenuItem>
+        </Header>
+      </div>
     );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,76 @@
 // @flow
 import * as React from 'react';
-import {
-  withRouter,
-  type ContextRouter
-} from 'react-router-dom';
-
+import { type ContextRouter } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 import './Header.css';
 
-type Props = ContextRouter;
+type Props = ContextRouter & {
+  children: React.ChildrenArray<React.Element<typeof MenuItem>>
+};
 
-class Header extends React.Component<Props, {}> {
+type State = {
+  drawerIsOpen: boolean
+}
+
+class Header extends React.Component<Props, State> {
   handleTitleClick: () => void;
+  handleMenuClick: () => void;
 
-  constructor(props) {
+  state = {
+    drawerIsOpen: false
+  };
+
+  constructor(props: Props) {
     super(props)
 
     this.handleTitleClick = this.handleTitleClick.bind(this);
+    this.handleMenuClick = this.handleMenuClick.bind(this);
   }
 
   handleTitleClick() {
     this.props.history.push('/');
   }
 
+  handleMenuClick() {
+    this.setState({
+      drawerIsOpen: !this.state.drawerIsOpen
+    });
+  }
+
   render() {
+    let items = React.Children.map(this.props.children, (child) => {
+      return (
+        <MenuItem
+          onClick={this.handleMenuClick}
+          {...child.props}
+        />
+      )
+    })
     return (
-      <AppBar
-        title={<span className='title'>PokeData</span>}
-        onTitleTouchTap={this.handleTitleClick}
-        iconElementRight={<IconButton><MoreVertIcon/></IconButton>}
-      />
+      <div>
+        <AppBar
+          title={<span className='title'>PokeData</span>}
+          onTitleTouchTap={this.handleTitleClick}
+          onLeftIconButtonTouchTap={this.handleMenuClick}
+          iconElementRight={<IconButton><MoreVertIcon/></IconButton>}
+        />
+        <Drawer
+          open={this.state.drawerIsOpen}
+          width={200}
+          docked={false}
+          onRequestChange={this.handleMenuClick}
+        >
+          {items}
+        </Drawer>
+      </div>
     );
   }
 }
 
-export default withRouter(Header);
+export default Header;
+// export { Header as PureHeader };
