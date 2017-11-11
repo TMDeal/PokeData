beaf6a66dfcd81b9f308fa0fbf2f7e692733ecea
Trent Deal <trent.deal@gmail.com>
2017-11-10 20:42:43 -0500

    Remove enableTracing setting because annoying

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { PokemonDetailComponent } from './pokemon/pokemon-detail/pokemon-detail.component';
-import { environment } from '../environments/environment';
 
 const routes: Routes = [
   { path: '', redirectTo: '/pokemon', pathMatch: 'full'},
@@ -10,18 +9,8 @@ const routes: Routes = [
   { path: 'pokemon/:name', component:  PokemonDetailComponent }
 ];
 
-export let routerSettings: ExtraOptions = {
-  enableTracing: false
-};
-
-if (!environment.production) {
-  routerSettings = {
-    enableTracing: true
-  };
-}
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes, routerSettings)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
